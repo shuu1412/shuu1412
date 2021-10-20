0bdd9339d8eb37c1b76877eabf1ba524dd0540a7
shuu1412 <sasuke22062002@gmail.com>
2021-10-20 18:08:28 +0700

    btvnb1

---
+++ Buổi 1/Bai1.py
@@ -0,0 +1,3 @@
+import math
+r=float(input("Nhập bán kính: "))
+print("S= ",math.pi*r*r,"\nP= ",math.pi*2*r)
---
+++ Buổi 1/Bai2.py
@@ -0,0 +1,3 @@
+import math
+x= float(input("Nhập x: "))
+print("f(x)= ",(x**2 + math.e**x+ math.sin(x))/math.sqrt(x**2+1))
